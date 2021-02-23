c65c772a9007cb40fd53eaa9f9fd488863e50881
Santiago Almiron <sanalmiron@hotmail.com>
2021-02-23 15:54:01 -0300

    as a user i can create a booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,27 @@
+class BookingsController < ApplicationController
+  before_action :set_castle, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.castle = @castle
+    @booking.user = current_user
+    @booking.save
+  end
+
+  private
+
+  def set_castle
+    @castle = Castle.find(params[:castle_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out, :total_price)
+  end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'booking/new'
+  get 'booking/create'
   devise_for :users
   root to: 'castles#index'
-  resources :castles, only: [:show]
+  resources :castles, only: [:show] do
+    resources :bookings, only: [:new, :create, :index]
+  end
+  resources :bookings, only: [:destroy]
 end
