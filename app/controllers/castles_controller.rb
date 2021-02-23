7cefab91caba88873644db5d4a1ecf8a6cd1f4a1
nataly garcia <naty_garcioli@hotmail.com>
2021-02-23 11:18:21 -0500

    resolve conflict

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,5 +1,13 @@
 class CastlesController < ApplicationController
 
+  def index
+    @castles = Castle.all
+  end
+
+  def new
+    @castle = Castle.new
+  end
+
   def show
     @castle = Castle.find(params[:id])
   end
