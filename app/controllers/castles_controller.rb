2656bccdf9693645b26a583a2de5bd37d613cb71
nataly garcia <naty_garcioli@hotmail.com>
2021-02-23 13:36:52 -0500

    add update method, corrected field from show/form

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,5 +1,6 @@
 class CastlesController < ApplicationController
-  
+  before_action :find_castle, only: [:show, :edit, :update, :destroy]
+
   def index
     @castles = Castle.all
   end
@@ -7,9 +8,25 @@ class CastlesController < ApplicationController
   def new
     @castle = Castle.new
   end
-  
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @castle.update(castle_params)
+    redirect_to @castle
+  end
+
+  private
+
+  def find_castle
     @castle = Castle.find(params[:id])
   end
-  
+
+  def castle_params
+    params.require(:castle).permit(:name, :country, :city, :address, :zip_code, :description, :price_per_day)
+  end
 end
