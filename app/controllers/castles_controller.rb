625002a52927b050a2d492652f760747e46ffdb0
emi rodriguez <emirodriguez_025@hotmail.com>
2021-02-23 14:18:36 -0300

    modify castle controller

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -6,4 +6,13 @@ class CastlesController < ApplicationController
   def new
     @castle = Castle.new
   end
+
+  def create
+    @castle = Castle.new(castle_params)
+    if @castle.save
+      redirect_to castle_path(@castle)
+    else
+      render 'new'
+    end
+  end
 end
