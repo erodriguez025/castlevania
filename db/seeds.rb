09b188d64701cdf60af50cc4373ae30e4046378e
Gonzalo Enei <gonzalo@fintual.com>
2021-02-23 13:32:16 -0300

    Adds Castle and User seeds and changes version of bundler gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Castle.destroy_all
+
+puts 'Creating 100 fake castles...'
+100.times do
+  castle = Castle.new(
+    name: Faker::Address.street_name,
+    country: Faker::Address.country,
+    city: Faker::Address.city,
+    address: Faker::Address.street_address,
+    zip_code: Faker::Address.zip_code,
+    description: Faker::Lorem.paragraph,
+    price_per_day: rand(199..2000)
+  )
+  castle.save!
+  puts "Castle saved"
+end
+puts 'Finished!'
+
+User.destroy_all
+
+puts 'Creating 100 fake users...'
+100.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: "Lamejorclave22",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    role: user
+  )
+  user.save!
+  puts "User saved"
+end
+puts 'Finished!'
